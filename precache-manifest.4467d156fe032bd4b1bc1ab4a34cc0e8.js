87b5c65fd4d21ace0fffda5037841c7a607681eb
samiroli <samiroli789@gmail.com>
2020-01-07 17:45:05 +0545

    Updates

---
+++ precache-manifest.4467d156fe032bd4b1bc1ab4a34cc0e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a918cd6986368b63c5ee6518af89ef",
+    "url": "/Todo-Application-ReactJS/index.html"
+  },
+  {
+    "revision": "f0373046dc9df8d5cf14",
+    "url": "/Todo-Application-ReactJS/static/css/main.810c634a.chunk.css"
+  },
+  {
+    "revision": "b6ead3c0d0684e98c1a1",
+    "url": "/Todo-Application-ReactJS/static/js/2.d631241a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo-Application-ReactJS/static/js/2.d631241a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0373046dc9df8d5cf14",
+    "url": "/Todo-Application-ReactJS/static/js/main.4f784cc8.chunk.js"
+  },
+  {
+    "revision": "c8465c50f5214a371198",
+    "url": "/Todo-Application-ReactJS/static/js/runtime-main.4d7e4a0f.js"
+  },
+  {
+    "revision": "bd1c9bd81e627ce01a8d68b9626d97cf",
+    "url": "/Todo-Application-ReactJS/static/media/emptyList.bd1c9bd8.png"
+  }
+]);
